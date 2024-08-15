b151610b616e733fc838dc4591f3cb4bfe560f1f
Samantha Callie <Hoomee90@gmail.com>
2024-08-15 13:34:18 -0700

    initial commit

---
+++ SweetTreats/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SweetTreats.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace SweetTreats
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddControllersWithViews();
+			
+			builder.Services.AddDbContext<SweetTreatsContext>(
+											dbContextOptions => dbContextOptions
+												.UseMySql(
+													builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
+												)
+											);
+			builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+											.AddEntityFrameworkStores<SweetTreatsContext>()
+											.AddDefaultTokenProviders();
+
+			WebApplication app = builder.Build();
+			
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			
+			app.UseRouting();
+			
+			app.UseAuthentication();
+			app.UseAuthorization();
+			
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}"
+			);
+
+			app.Run();
+		}
+	}
+}
