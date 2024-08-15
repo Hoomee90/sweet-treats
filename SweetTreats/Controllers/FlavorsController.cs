4db435e0a060fdcecca8e84fc0b83383c2cb8599
Samantha Callie <Hoomee90@gmail.com>
2024-08-15 15:02:22 -0700

    add sweet and flavor controllers

---
+++ SweetTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,125 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SweetTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace SweetTreats.Controllers
+{
+	[Authorize]
+	public class FlavorsController : Controller
+	{
+		private readonly SweetTreatsContext _db;
+		private readonly UserManager<ApplicationUser> _userManager;
+		
+		public FlavorsController(UserManager<ApplicationUser> userManager, SweetTreatsContext db)
+		{
+			_userManager = userManager;
+			_db = db;
+		}
+		public async Task<ActionResult> Index()
+		{
+			string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+			List<Flavor> userFlavors = _db.Flavors
+				.Where(entry => entry.User.Id == currentUser.Id)
+				.ToList();
+			return View(userFlavors);
+		}
+		
+		[AllowAnonymous]
+		public ActionResult Details(int id)
+		{
+			Flavor thisFlavor = _db.Flavors
+				.Include(flavor => flavor.JoinEntities)
+				.ThenInclude(join => join.Sweet)
+				.FirstOrDefault(flavor => flavor.FlavorId == id);
+			return View(thisFlavor);
+		}
+		
+		public ActionResult Create()
+		{
+			return View();
+		}
+		
+		[HttpPost]
+		public async Task<ActionResult> Create(Flavor flavor)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(flavor);
+			}
+			else
+			{
+				string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+				ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+				flavor.User = currentUser;
+				_db.Flavors.Add(flavor);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+		}
+		
+		public ActionResult AddSweet(int id)
+		{
+			Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+			ViewBag.SweetId = new SelectList(_db.Sweets, "SweetId", "Name");
+			return View(thisFlavor);
+		}
+		
+		[HttpPost]
+		public ActionResult AddSweet(Flavor flavor, int sweetId)
+		{
+			bool joinEntityExists = _db.SweetFlavors.Any(join => join.SweetId == sweetId && join.FlavorId == flavor.FlavorId);
+			if (!joinEntityExists && sweetId != 0)
+			{
+				_db.SweetFlavors.Add(new SweetFlavor() { SweetId = sweetId, FlavorId = flavor.FlavorId });
+				_db.SaveChanges();
+			}
+			return RedirectToAction("Details", new { id = flavor.FlavorId});
+		}
+		
+		public ActionResult Edit(int id)
+		{
+			Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+			return View(thisFlavor);
+		}
+		
+		[HttpPost]
+		public ActionResult Edit(Flavor flavor)
+		{
+			_db.Flavors.Update(flavor);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		
+		public ActionResult Delete(int id)
+		{
+			Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+			return View(thisFlavor);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+			_db.Flavors.Remove(thisFlavor);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		
+		[HttpPost]
+		public ActionResult DeleteJoin(int joinId)
+		{
+			SweetFlavor joinEntry = _db.SweetFlavors.FirstOrDefault(entry => entry.SweetFlavorId == joinId);
+			_db.SweetFlavors.Remove(joinEntry);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ SweetTreats/Controllers/TreatsController.cs
@@ -0,0 +1,154 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SweetTreats.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace SweetTreats.Controllers
+{
+	[Authorize]
+	public class SweetsController : Controller
+	{
+		private readonly SweetTreatsContext _db;
+		private readonly UserManager<ApplicationUser> _userManager;
+		
+		public SweetsController(UserManager<ApplicationUser> userManager, SweetTreatsContext db)
+		{
+			_userManager = userManager;
+			_db = db;
+		}
+		
+		public async Task<ActionResult> Index(string sort, string searchString)
+		{
+			ViewBag.NameSortParm = string.IsNullOrEmpty(sort) ? "name_desc" : "";
+			ViewBag.PriceSortParm = sort == "price" ? "price_desc" : "price";
+			
+			string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+			var userSweets = from r in _db.Sweets.Where(entry => entry.User.Id == currentUser.Id)
+												select r;
+			if (!string.IsNullOrEmpty(searchString))
+			{
+				userSweets = userSweets.Where(r => r.Name.Contains(searchString));
+			}
+			switch (sort)
+			{
+				case "name_desc":
+				userSweets = userSweets.OrderByDescending(r => r.Name);
+				break;
+			case "price":
+				userSweets = userSweets.OrderBy(r => r.Price);
+				break;
+			case "price_desc":
+				userSweets = userSweets.OrderByDescending(r => r.Price);
+				break;
+			default:
+				userSweets = userSweets.OrderBy(r => r.Name);
+				break;
+			}
+
+			return View(userSweets.ToList());
+		}
+		
+		public ActionResult Create()
+		{
+			return View();
+		}
+		
+		[HttpPost]
+		public async Task<ActionResult> Create(Sweet sweet)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(sweet);
+			}
+			else
+			{
+				string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+				ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+				sweet.User = currentUser;
+				_db.Sweets.Add(sweet);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+		}
+		
+		[AllowAnonymous]
+		public ActionResult Details(int id)
+		{
+			Sweet thisSweet = _db.Sweets
+				.Include(sweet => sweet.JoinEntities)
+				.ThenInclude(join => join.Flavor)
+				.FirstOrDefault(sweet => sweet.SweetId == id);
+			return View(thisSweet);
+		}
+		
+		public ActionResult Edit(int id)
+		{
+			Sweet thisSweet = _db.Sweets.FirstOrDefault(sweet => sweet.SweetId == id);
+			return View(thisSweet);
+		}
+		
+		[HttpPost]
+		public ActionResult Edit(Sweet sweet)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(sweet);
+			}
+			else
+			{
+				_db.Sweets.Update(sweet);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+		}
+		
+		public ActionResult AddFlavor(int id)
+		{
+			Sweet thisSweet = _db.Sweets.FirstOrDefault(sweet => sweet.SweetId == id);
+			ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+			return View(thisSweet);
+		}
+		
+		[HttpPost]
+		public ActionResult AddFlavor(Sweet sweet, int flavorId)
+		{
+			bool joinEntityExists = _db.SweetFlavors.Any(join => join.FlavorId == flavorId && join.SweetId == sweet.SweetId);
+			if (!joinEntityExists && flavorId != 0)
+			{
+				_db.SweetFlavors.Add(new SweetFlavor() { FlavorId = flavorId, SweetId = sweet.SweetId });
+				_db.SaveChanges();
+			}
+			return RedirectToAction("Details", new { id = sweet.SweetId });
+		}
+		
+		public ActionResult Delete(int id)
+		{
+			Sweet thisSweet = _db.Sweets.FirstOrDefault(sweet => sweet.SweetId == id);
+			return View(thisSweet);
+		}
+		
+		[HttpPost, ActionName("Delete")]
+		public ActionResult DeleteConfirmed(int id)
+		{
+				Sweet thisSweet = _db.Sweets.FirstOrDefault(sweet => sweet.SweetId == id);
+				_db.Sweets.Remove(thisSweet);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+		}
+		
+		[HttpPost]
+		public ActionResult DeleteJoin(int joinId)
+		{
+			SweetFlavor joinEntry = _db.SweetFlavors.FirstOrDefault(entry => entry.SweetFlavorId == joinId);
+			_db.SweetFlavors.Remove(joinEntry);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
